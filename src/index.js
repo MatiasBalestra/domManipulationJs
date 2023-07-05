ce50bd86fdce6d26f06752e639d3fc3f89ddbff2
Matias Balestra <mbalestra@applica.dev>
2023-07-04 23:00:25 -0300

    dom manipulation with js vanilla

--- src/index.js
+++ src/index.js
@@ -3,4 +3,56 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+console.log('Happy hacking :) Rusher Kun')
+
+const baseUrl= "https://platzi-avo.vercel.app"
+
+const appNode = document.querySelector('#app');
+
+//intl
+
+// 1 - format fechas
+
+// 2 - format monedas
+const formatPrice = (price) => {
+
+    const newPrice = new window.Intl.NumberFormat('es', 
+    {style:'currency',
+     currency:'ARS'}).format(price)
+
+    return newPrice;
+}
+
+
+//web api
+// conectarnos al servidor
+window.fetch(`${baseUrl}/api/avo`)
+// procesar la repuesta y convertirla en json
+.then(respuesta => respuesta.json())
+// json -> data -> renderizar info en el browser
+.then( responseJson=> {
+    responseJson.data.forEach(item => {
+
+        const todosLosItems = [];
+
+        // crear imagen
+        const imagen = document.createElement('img');
+        imagen.src = `${baseUrl}${item.image}`;
+        // crear titulo
+        const title = document.createElement('h2');
+        title.textContent= item.name;
+        title.className='text-xl text-red-600 text-center'
+        //crear precio
+        const price = document.createElement('div');
+        price.textContent= formatPrice(item.price);
+        price.className='text-center';
+
+        const container = document.createElement('div');
+        container.append(imagen, title, price);
+        container.className='shadow-xl';
+
+        todosLosItems.push(container);
+
+        appNode.append(...todosLosItems);
+    });
+})
